157251e5e0ea71e805598adad57fffe508ac9fe9
Randsw <randsw@mail.ru>
2025-03-14 19:57:25 +0300

    feat: Add curl example to add schema to schema registry

--- kafka-cluster.sh
+++ kafka-cluster.sh
@@ -225,3 +225,19 @@ spec:
   listener: tls
 EOF
 
+# echo '{
+#   "type": "object",
+#   "properties": {
+#     "user": {
+#       "type": "string"
+#     },
+#     "car": {
+#       "type": "string"
+#     },
+#     "color": {
+#       "type": "string"
+#     }
+#   }
+# }' | \
+# jq '. | {schema: tojson, schemaType: "JSON"}' | \
+# curl -X POST "https://schema.kind.cluster/subjects/example-topic/versions" -H "Content-Type:application/json"  -d @- 
